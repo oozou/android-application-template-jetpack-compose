f8935e607148169cca0cb31e5e760fd9d2f67023
Watsapon Kampong <51149687+watsaponk@users.noreply.github.com>
2023-05-08 16:41:41 +0700

    chore: Add Ktlint (#1)
    * chore: add ktlint-gradle main plugin
    
    * chore: add ktlint-idea plugins
    
    * style: format by ktlint
    
    * chore: add ktlint-idea plugins
    
    * chore: add addKtlintCheckGitPreCommitHook before preBuild

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,12 @@
 plugins {
     id("com.android.application") version "8.1.0-beta01" apply false
     id("org.jetbrains.kotlin.android") version "1.8.0" apply false
+    id("org.jlleitschuh.gradle.ktlint") version "11.3.2"
+    id("org.jlleitschuh.gradle.ktlint-idea") version "11.3.2"
 }
+
+allprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+}
+
+tasks.getByPath(":app:preBuild").dependsOn(tasks.addKtlintCheckGitPreCommitHook)
