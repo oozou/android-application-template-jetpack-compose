6198d564b13c4ea2fb33a7b3d4fd7c3c4b4c851c
Abdulrohim 'Him' Sama <a.rohim.sama@gmail.com>
2023-05-09 13:42:33 +0700

    Replaced app dependencies constants with gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,11 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
-    id("com.android.application") version "8.1.0-beta01" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.0" apply false
-    id("org.jlleitschuh.gradle.ktlint") version "11.3.2"
-    id("org.jlleitschuh.gradle.ktlint-idea") version "11.3.2"
-    id("com.android.library") version "8.1.0-beta01" apply false
+    alias(libs.plugins.com.android.application) apply false
+    alias(libs.plugins.org.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.org.jlleitschuh.gradle.ktlint)
+    alias(libs.plugins.org.jlleitschuh.gradle.ktlint.idea)
+    alias(libs.plugins.com.android.library) apply false
 }
 
 allprojects {
