d54dc4535cef59226eb48aebe848e695ded1c79c
Abdulrohim 'Him' Sama <a.rohim.sama@gmail.com>
2023-05-09 13:56:55 +0700

    Get build number from github var env

--- buildSrc/src/main/java/AppConfig.kt
+++ buildSrc/src/main/java/AppConfig.kt
@@ -3,6 +3,7 @@ object AppConfig {
     const val compileSdk = 33
     const val minSdk = 24
     const val targetSdk = 33
-    const val versionCode = 1
+    val buildNumber = (System.getenv("GITHUB_RUN_NUMBER") ?: "1").toString().toInt()
+    val versionCode = buildNumber
     const val versionName = "1.0.0"
 }
