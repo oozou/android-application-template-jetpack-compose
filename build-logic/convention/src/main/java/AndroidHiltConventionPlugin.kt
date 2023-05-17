7e3d07253198cf6f6296c54dd7d744ab3c103fff
Watsapon Kampong <51149687+watsaponk@users.noreply.github.com>
2023-05-17 16:55:39 +0700

    fix: solve linking resource issue (#13)

--- build-logic/convention/src/main/java/AndroidHiltConventionPlugin.kt
+++ build-logic/convention/src/main/java/AndroidHiltConventionPlugin.kt
@@ -8,8 +8,8 @@ class AndroidHiltConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
             with(pluginManager) {
-                apply("dagger.hilt.android.plugin")
-                apply("org.jetbrains.kotlin.kapt")
+                apply("kotlin-kapt")
+                apply("com.google.dagger.hilt.android")
             }
 
             val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
